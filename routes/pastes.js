b8fbada6b72c2b31646309de6c9e4621d86857fe
Nithishkumar4881 <nithisk72@gmail.com>
2026-01-27 17:43:19 +0530

    response changed to html

--- routes/pastes.js
+++ routes/pastes.js
@@ -15,7 +15,7 @@ router.post('/pastes', async (req, res) => {
       maxViews
     });
     await paste.save();
-    res.status(201).json({ content: paste.content, url: `http://localhost:3000/api/pastes/${paste._id}` });
+    res.status(201).json({ content: paste.content, url: `https://paste-bin-lite-iota.vercel.app/api/pastes/${paste._id}` });
   } catch (err) {
     res.status(500).json({ error: 'Failed to create paste', details: err.message });
   }
